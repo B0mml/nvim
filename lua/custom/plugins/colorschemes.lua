d2e60cdd3129b6a6c7a169014550bafa33421d70
Lucas Deichsel <luc_dei@uni-bremen.de>
2025-07-03 17:56:24 +0200

    add onedark

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -5,10 +5,22 @@ return {
     'thesimonho/kanagawa-paper.nvim',
     lazy = false,
     priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'kanagawa-paper-ink'
-    end,
+    -- init = function()
+    --   vim.cmd.colorscheme 'kanagawa-paper-ink'
+    -- end,
     opts = { ... },
+  },
+  -- Using Lazy
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker',
+      }
+      -- Enable theme
+      require('onedark').load()
+    end,
   },
   {
     'webhooked/kanso.nvim',
