738e448d918d6a34ef6f781cfe7e593ddf8f1318
Lucas Deichsel <luc_dei@uni-bremen.de>
2025-07-07 14:33:28 +0200

    change console exit keybind

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -10,7 +10,6 @@ return {
     -- end,
     opts = { ... },
   },
-  -- Using Lazy
   {
     'navarasu/onedark.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
