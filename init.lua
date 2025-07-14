f7d8b31fc777404efc0f127b5269c354cbaeba0f
Lucas Deichsel <luc_dei@uni-bremen.de>
2025-07-14 14:13:41 +0200

    add stylua config

--- init.lua
+++ init.lua
@@ -614,6 +614,11 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        stylua = {
+          prepend_args = { '--collapse-simple-statement', 'Always' },
+        },
+      },
     },
   },
 
