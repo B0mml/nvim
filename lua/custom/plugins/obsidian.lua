99a120a710104dde0e0d373047af1503ecb93bfe
lucas <lu.deichsel@gmail.com>
2025-08-07 03:32:19 +0200

    add obsidian

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,60 +1,134 @@
 return {
-  -- 'obsidian-nvim/obsidian.nvim',
-  -- version = '*', -- recommended, use latest release instead of latest commit
-  -- lazy = true,
-  -- ft = 'markdown',
-  -- -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- -- event = {
-  -- --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  -- --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  -- --   -- refer to `:h file-pattern` for more examples
-  -- --   "BufReadPre path/to/my-vault/*.md",
-  -- --   "BufNewFile path/to/my-vault/*.md",
-  -- -- },
-  -- dependencies = {
-  --   -- Required.
-  --   'nvim-lua/plenary.nvim',
-  --   'snacks.nvim',
-  --   'Saghen/blink.cmp',
-  --   -- see above for full list of optional dependencies ☝️
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
   -- },
-  -- ---@module 'obsidian'
-  -- ---@type obsidian.config
-  -- opts = {
-  --   workspaces = {
-  --     {
-  --       name = 'My Vault',
-  --       path = '~/Documents/My Vault/',
-  --     },
-  --   },
-  --   -- see below for full list of options 👇
-  --   completion = {
-  --     nvim_cmp = false,
-  --     blink = true,
-  --     min_chars = 2,
-  --   },
-  -- },
-  -- config = function(_, opts)
-  --   require('obsidian').setup(opts)
-  --
-  --   -- Keymaps are only created when the plugin loads
-  --   -- Normal mode keymaps
-  --   vim.keymap.set('n', '<leader>oo', '<cmd>ObsidianOpen<CR>', { desc = 'Open in Obsidian app' })
-  --   vim.keymap.set('n', '<leader>oq', '<cmd>ObsidianQuickSwitch<CR>', { desc = 'Quick switch note' })
-  --   vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<CR>', { desc = 'Create new note' })
-  --   vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianToday<CR>', { desc = "Today's daily note" })
-  --   vim.keymap.set('n', '<leader>oy', '<cmd>ObsidianYesterday<CR>', { desc = "Yesterday's daily note" })
-  --   vim.keymap.set('n', '<leader>ob', '<cmd>ObsidianBacklinks<CR>', { desc = 'Show backlinks' })
-  --   vim.keymap.set('n', '<leader>os', '<cmd>ObsidianSearch<CR>', { desc = 'Search vault' })
-  --   vim.keymap.set('n', '<leader>ol', '<cmd>ObsidianLink<CR>', { desc = 'Link visual text to note' })
-  --   vim.keymap.set('n', '<leader>of', '<cmd>ObsidianFollowLink<CR>', { desc = 'Follow link under cursor' })
-  --   vim.keymap.set('n', '<leader>ow', '<cmd>ObsidianWorkspace<CR>', { desc = 'Switch workspace' })
-  --   vim.keymap.set('n', '<leader>od', '<cmd>ObsidianDailies<CR>', { desc = 'Browse daily notes' })
-  --   vim.keymap.set('n', '<leader>otc', '<cmd>ObsidianTOC<CR>', { desc = 'Table of contents' })
-  --
-  --   -- Visual mode keymaps
-  --   vim.keymap.set('v', '<leader>ox', ':ObsidianExtractNote<CR>', { desc = 'Extract selection to new note' })
-  --   vim.keymap.set('v', '<leader>ol', ':ObsidianLink<CR>', { desc = 'Link visual text' })
-  --   vim.keymap.set('v', '<leader>on', ':ObsidianLinkNew<CR>', { desc = 'Create and link new note from selection' })
-  -- end,
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+    'snacks.nvim',
+    'Saghen/blink.cmp',
+    -- see above for full list of optional dependencies ☝️
+  },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = 'Vault',
+        path = '~/obsidian/Vault/',
+      },
+    },
+
+    -- Configure note ID generation to use title
+    note_id_func = function(title)
+      if title ~= nil then
+        -- Clean title: lowercase, replace spaces with hyphens, remove special chars
+        return title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- Fallback to timestamp if no title
+        return tostring(os.time())
+      end
+    end,
+
+    -- Configure note path to use cleaned title as filename
+    note_path_func = function(spec)
+      local title = spec.title or spec.id
+      local clean_title = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      return spec.dir / (clean_title .. '.md')
+    end,
+
+    templates = {
+      folder = 'templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+      substitutions = {
+        datetime = function() return os.date '%Y-%m-%d %H:%M' end,
+      },
+    },
+
+    completion = {
+      nvim_cmp = false,
+      blink = true,
+      min_chars = 2,
+    },
+  },
+  config = function(_, opts)
+    require('obsidian').setup(opts)
+
+    local function map(mode, lhs, rhs, desc) vim.keymap.set(mode, lhs, rhs, { desc = desc, silent = true }) end
+    -- Core note operations
+    map('n', '<leader>on', '<cmd>ObsidianNew<cr>', 'New note')
+    map('n', '<leader>oN', '<cmd>ObsidianNewFromTemplate<cr>', 'New note from template')
+    map('n', '<leader>oo', '<cmd>ObsidianOpen<cr>', 'Open note')
+    map('n', '<leader>oq', '<cmd>ObsidianQuickSwitch<cr>', 'Quick switch notes')
+
+    -- Search and find
+    map('n', '<leader>of', '<cmd>ObsidianSearch<cr>', 'Search notes')
+    map('n', '<leader>ob', '<cmd>ObsidianBacklinks<cr>', 'Show backlinks')
+    map('n', '<leader>ol', '<cmd>ObsidianLinks<cr>', 'Show all links')
+    map('n', '<leader>ot', '<cmd>ObsidianTags<cr>', 'Browse tags')
+
+    -- Daily notes
+    map('n', '<leader>od', '<cmd>ObsidianToday<cr>', "Today's note")
+    map('n', '<leader>oy', '<cmd>ObsidianYesterday<cr>', "Yesterday's note")
+    map('n', '<leader>om', '<cmd>ObsidianTomorrow<cr>', "Tomorrow's note")
+    map('n', '<leader>oD', '<cmd>ObsidianDailies<cr>', 'Browse daily notes')
+
+    -- Link operations
+    map('n', '<leader>oL', '<cmd>ObsidianLink<cr>', 'Create link from selection')
+    map('v', '<leader>oL', '<cmd>ObsidianLink<cr>', 'Create link from selection')
+    map('n', '<leader>oF', '<cmd>ObsidianLinkNew<cr>', 'Create new note and link')
+    map('v', '<leader>oF', '<cmd>ObsidianLinkNew<cr>', 'Create new note and link')
+    map('n', '<leader>og', '<cmd>ObsidianFollowLink<cr>', 'Follow link under cursor')
+
+    -- Templates and workspace
+    map('n', '<leader>oT', '<cmd>ObsidianTemplate<cr>', 'Insert template')
+    map('n', '<leader>ow', '<cmd>ObsidianWorkspace<cr>', 'Switch workspace')
+
+    -- Utility
+    map('n', '<leader>or', '<cmd>ObsidianRename<cr>', 'Rename note')
+    map('n', '<leader>ox', '<cmd>ObsidianExtractNote<cr>', 'Extract note from selection')
+    map('v', '<leader>ox', '<cmd>ObsidianExtractNote<cr>', 'Extract note from selection')
+    map('n', '<leader>op', '<cmd>ObsidianPasteImg<cr>', 'Paste image')
+    map('n', '<leader>oc', '<cmd>ObsidianToggleCheckbox<cr>', 'Toggle checkbox')
+
+    -- Advanced API shortcuts (using Lua functions)
+    map('n', '<leader>oS', function()
+      local client = require('obsidian').get_client()
+      local picker = client:picker()
+      if picker then picker:find_notes() end
+    end, 'Smart note finder')
+
+    map('n', '<leader>oR', function()
+      local client = require('obsidian').get_client()
+      local note = client:current_note()
+      if note then
+        local backlinks = client:find_backlinks(note)
+        if #backlinks > 0 then
+          vim.notify('Found ' .. #backlinks .. ' backlinks')
+        -- You could open a picker here to select from backlinks
+        else
+          vim.notify 'No backlinks found'
+        end
+      end
+    end, 'Find backlinks for current note')
+
+    map('n', '<leader>oi', function()
+      local client = require('obsidian').get_client()
+      local note = client:current_note()
+      if note then
+        local info = note:display_info()
+        vim.notify(info, vim.log.levels.INFO)
+      end
+    end, 'Show note info')
+  end,
 }
